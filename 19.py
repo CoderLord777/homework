a3db55a8fdb4eb07a16e893d5997b817baf9a8f1
CoderLord777 <vovbub@gmail.com>
2024-09-22 21:51:50 +0300

    Delete 19.py

--- 19.py
+++
@@ -1,8 +0,0 @@
-def f(k,h):
-    if k>=202: return h%2==0
-    if h==0: return 0
-    xod = [f(k+1,h-1),f(k+4,h-1),f(k*3,h-1)]
-    return any (xod) if (h-1)%2==0 else all (xod)
-print(min (w for w in range (1,202) if f(w,2)))
-print ([w for w in range (1,202) if f(w,3)>f(w,1)])
-print(min (w for w in range (1,202) if f(w,4)>f(w,2)))
