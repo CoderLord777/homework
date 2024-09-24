2c60b9d44c3d19b07911d59e84090d4ed3eccf12
Lord777 <Lord_777@inbox.ru>
2024-09-24 22:03:08 +0300

    24/09

---
+++ base.py
@@ -0,0 +1,20 @@
+print("1st program")
+print(9**0.5*5)
+print("2nd program")
+if 9.99>9.98 and 1000!=1000.1:
+    print(True)
+print("3rd program")
+print(2*2+2)
+print(2*(2+2))
+if 2*2+2==2*(2+2):
+    print(True)
+else:
+    print(False)
+print("4th program")
+a= '123.456'
+print(a[-3])
+a=float(a)
+a*=10
+print(a)
+a=int(a)
+print(a//10)
