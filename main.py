223ed2674df03bc079eddc951a8db8f6f117be2b
Lord777 <Lord_777@inbox.ru>
2024-09-24 22:03:57 +0300

    24/09

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("1st program")
+print(9**0.5*5)
+print("2nd program")
+if 9.99>9.98 and 1000!=1000.1:
+    print(True)
+print("3rd program")
+print(2*2+2)
+print(2*(2+2))
+if 2*2+2==2*(2+2):
+    print(True)
+else:
+    print(False)
+print("4th program")
+a= '123.456'
+print(a[-3])
+a=float(a)
+a*=10
+print(a)
+a=int(a)
+print(a//10)
