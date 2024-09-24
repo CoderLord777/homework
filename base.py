277a743559c1aa304d58e0553fe6114b96cae8a9
CoderLord777 <vovbub@gmail.com>
2024-09-24 22:17:27 +0300

    Add files via upload

---
+++ base.py
@@ -0,0 +1,16 @@
+print("1st program")
+print(9**0.5*5)
+print("2nd program")
+if 9.99>9.98 and 1000!=1000.1:
+    print(True)
+print("3rd program")
+print(2*2+2)
+print(2*(2+2))
+if 2*2+2==2*(2+2):
+    print(True)
+else:
+    print(False)
+print("4th program")
+a= '123.456'
+print(a[-3])
+print(int(float(a*10%10)))
