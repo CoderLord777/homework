150ccf3a1a8fca556f5eea8b13799ba1b6b0119c
Lord777 <Lord_777@inbox.ru>
2024-10-21 20:14:07 +0300

    24/09

--- module_4_1.py
+++ module_4_1.py
@@ -1,1 +1,10 @@
-import thrue_math as th
+import true_math as th
+import fake_math as fake
+norm = th.divide(58,8)
+norm2 = th.divide(5,0)
+ne_norm = fake.divide(71,9)
+ne_norm2 = fake.divide(6,0)
+print(norm)
+print(norm2)
+print(ne_norm)
+print(ne_norm2)
