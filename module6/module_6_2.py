73f303d1dac7acf3cc8494422843c61e8af8d23b
Lord777 <Lord_777@inbox.ru>
2024-11-10 12:42:58 +0300

    24/09

--- module6/module_6_2.py
+++ module6/module_6_2.py
@@ -1,10 +1,12 @@
 class Vehicle:
-    __COLOR_VARIANTS = ['Аустер', 'Гейзер', 'Индиго', '	Лагуна','Лазурь']
-    def __init__(self, owner: str, __model: str,  __engine_power: int,  __color: str):
+    __COLOR_VARIANTS = ['Аустер', 'Гейзер', 'Индиго', '	Лагуна', 'Лазурь']
+
+    def __init__(self, owner: str, __model: str, __engine_power: int, __color: str):
         self.owner = owner
         self.__model = __model
         self.__engine_power = __engine_power
         self.__color = __color
+
     def get_model(self):
         return f'{self.__model}'
 
@@ -13,11 +15,13 @@ class Vehicle:
 
     def get_color(self):
         return f'{self.__color}'
+
     def print_info(self):
         print(f'Модель: {self.get_model()}')
         print(f'Мощность двигателя: {self.get_horsepower()}')
         print(f'Цвет: {self.get_color()}')
-        print (f'Владелец: {self.owner}')
+        print(f'Владелец: {self.owner}')
+
     def set_color(self, new_color: str):
         new_color = new_color
         if new_color in self.__COLOR_VARIANTS:
@@ -25,10 +29,12 @@ class Vehicle:
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
+
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500,'blue')
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
 # Изначальные свойства
 vehicle1.print_info()
 # Меняем свойства (в т.ч. вызывая методы)
@@ -37,4 +43,3 @@ vehicle1.set_color('Индиго')
 vehicle1.owner = 'Vasyok'
 # Проверяем что поменялось
 vehicle1.print_info()
-
